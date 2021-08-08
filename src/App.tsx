ab84b7b913076a17a6e3d08afbae84751362ae63
horisanLog <ttie7374@gmail.com>
2021-08-08 13:46:50 +0900

    handleInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,91 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css';
-import Data from "./data.json"
-import TestComponent from './testComponent';
+import React from "react"
+import "./App.css"
+// import Data from "./data.json"
+import TestComponent from "./testComponent"
 
-type USERS = typeof Data
+// type USERS = typeof Data
 
-//INtersection types
-type PROFILE = {
-  age: Number,
-  city: string
-}
+// //INtersection types
+// type PROFILE = {
+//   age: number
+//   city: string
+// }
 
-type LOGIN = {
-  username: string,
-  password: string
-}
+// type LOGIN = {
+//   username: string
+//   password: string
+// }
 
-type USER = PROFILE & LOGIN
+// type USER = PROFILE & LOGIN
 
-const userA: USER = {
-  age: 30,
-  city: "tokyo",
-  username: "xxx",
-  password: "yyy",
-}
+// const userA: USER = {
+//   age: 30,
+//   city: "tokyo",
+//   username: "xxx",
+//   password: "yyy",
+// }
 
-// Union Types
-let value: boolean | number
-value = true
-value = 10
+// // Union Types
+// let value: boolean | number
+// value = true
+// value = 10
 
-let arrayUni: (number | string | boolean)[]
-arrayUni = [0, 1, 2, "hello", true]
+// // it's never initialized.
+// let arrayUni: (number | string | boolean)[]
+// arrayUni = [0, 1, 2, "hello", true]
 
-// Literal Types
-let company: "facebook" | "Google" | "Amazon"
-company = "Amazon"
+// // Literal Types
+// let company: "facebook" | "Google" | "Amazon"
+// company = "Amazon"
 
-// typeof
-let msg: string = "hi"
-let msg2: typeof msg
+// // typeof
+// let msg: string = "hi"
+// let msg2: typeof msg
 
-msg2 = "hello"
+// msg2 = "hello"
 
-let animal = {cat: "small cat"}
-let newAnimal: typeof animal = {cat: "big cat"}
+// let animal = { cat: "small cat" }
+// let newAnimal: typeof animal = { cat: "big cat" }
 
-// keyof
-type KEYS = {
-  primary: string
-  secondary: string
-}
+// // keyof
+// type KEYS = {
+//   primary: string
+//   secondary: string
+// }
 
-let key: keyof KEYS
-key = "primary"
+// let key: keyof KEYS
+// key = "primary"
 
-// typeof + keyof
-const SPORTS = {
-  soccer: "soccer",
-  baseball: "baseball"
-}
+// // typeof + keyof
+// const SPORTS = {
+//   soccer: "soccer",
+//   baseball: "baseball",
+// }
 
-//Generics
-interface GEN<T>{
-  item: T
-}
+// //Generics
+// interface GEN<T> {
+//   item: T
+// }
 
-const gen0: GEN<string> = { item: "hello" }
-const gen1: GEN<number> = { item: 1 }
+// const gen0: GEN<string> = { item: "hello" }
+// const gen1: GEN<number> = { item: 1 }
 
-interface GEN1<T extends string | number > {
-  item: T
-}
+// interface GEN1<T extends string | number> {
+//   item: T
+// }
 
-const gen2: GEN1<string> = { item: "hi"}
+// const gen2: GEN1<string> = { item: "hi" }
 
-function funcGEN<T>(props: T) {
-  return { item: props }
-}
+// function funcGEN<T>(props: T) {
+//   return { item: props }
+// }
 
-const gen3 = funcGEN<string>("test")
+// const gen3 = funcGEN<string>("test")
 
-function funcGEN1<T extends string | null>(props: T) {
-  return { value: props}
-}
-const gen4 = funcGEN1("hello")
+// function funcGEN1<T extends string | null>(props: T) {
+//   return { value: props }
+// }
+// const gen4 = funcGEN1("hello")
 
 // interface Props {
 //   price: number
@@ -105,7 +105,7 @@ export const App: React.FC = () => {
         <TestComponent text="hello"/>
       </header>
     </div>
-  );
+  )
 }
 
 export default App
--- src/testComponent.tsx
+++ src/testComponent.tsx
@@ -1,15 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 
 interface Props {
-   text: string
+  text: string
 }
 
 export const testComponent: React.FC<Props> = (props) => {
+  const [inputData, setInputData] = useState("")
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData(e.target.value)
+  }
+
   return (
     <div>
       <h1>{props.text}</h1>
+      <input type="text" value={inputData} onChange={handleInput} />
+      <h1>{inputData}</h1>
     </div>
-  );
+  )
 }
 
 export default testComponent
