087f150cff8cd023ab9ccb454d79fc7c3201ad91
horisanLog <ttie7374@gmail.com>
2021-08-08 09:17:53 +0900

    INtersection_types

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,26 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css';
 
+//INtersection types
+type PROFILE = {
+  age: Number,
+  city: string
+}
+
+type LOGIN = {
+  username: string,
+  password: string
+}
+
+type USER = PROFILE & LOGIN
+
+const userA: USER = {
+  age: 30,
+  city: "tokyo",
+  username: "xxx",
+  password: "yyy",
+}
+
 export const App:React.FC = () => {
   return (
     <div className="App">
